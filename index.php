9535174207d426bcf5c18b1304a25ad1f256c0fc
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-03-27 22:15:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,39 +4,30 @@ $servidor = 'localhost';
 $usuario  = 'root';
 $senha    = '';
 $banco    = 'bot';
-$conn     = mysqli_connect($servidor,$usuario,$senha,$banco);
+$conn     = mysqli_connect($servidor, $usuario, $senha, $banco);
 
-if(!$conn){
+if (!$conn) {
+    // se der erro na conexão
+    die("Erro na conexão do BD: " . mysqli_connect_error());
+    
+} else {
 
-}else{
+    // Conectado recebendo os valores de numero e a ultima mensagem enviada do cliente
+    $telefone = $_GET['telefone'];
+    $msg      = $_GET['msg'];
 
-}
+    //cadastrar no banco de dados se nao estiver cadastrado
 
-#coletando variaveis
-$telefone = $_GET['telefone'];
-$msg      = $_GET['msg'];
-#echo "Ola como vai ? $telefone ultima mensagem: $msg <br>";
+    $sql_verificar = "SELECT * FROM usuario WHERE telefone = '$telefone'";
+    $result_verificar = $conn->query($sql_verificar);
 
-$sql = "SELECT * FROM usuario WHERE telefone = '$telefone'";
-$query = mysqli_query($conn,$sql);
-$total = mysqli_num_rows($query);
-
-if($total == 0){
-
-    $sql = "INSERT INTO usuario (telefone,status) VALUES ('$telefone', '1')";
-    $query = mysqli_query($conn,$sql);
-    if($query){
-
-        echo "seu numero foi salvo no meu banco de dados";    
-        
+    if(!$result_verificar){
+        $sql = "INSERT INTO usuario (telefone) VALUES ('$telefone')";
+        $query = mysqli_query($conn,$sql);
+        echo"1";
+    } else{
+        echo"2";
     }
 
 }
-
-################################################################################################################
-
-if($total == 1){
-
-}
-
 ?>
