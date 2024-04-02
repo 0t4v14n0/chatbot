e188ee507bf2d5bf6c89cce791936e550af7d256
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-04-02 01:52:55 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ import time
 import requests
 
 #facilitar munutencao
-bolinha_not    = ('lyutrhe2')
+bolinha_not    = ('x7h3shv')
 contato_cli    = ('//*[@id="main"]/header/div[2]/div/div/div/span')
-msg_cliente    = ('_21Ahp')
+msg_cliente    = ('_akbu')
 caixa_msg      = ('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
 caixa_msg2     = ('div[title="Digite uma mensagem"]')
 caixa_pesquisa = ('div[title="Caixa de texto de pesquisa"]')
@@ -21,7 +21,7 @@ chrome_options2 = Options()
 chrome_options2.add_argument(r"user-data-dir=" + dir_path + "/zap/sessao")
 driver = webdriver.Chrome(options=chrome_options2)
 driver.get('https://web.whatsapp.com/')
-time.sleep(30)
+time.sleep(5)
 
 def bot():
     try:
@@ -35,28 +35,31 @@ def bot():
         acao_bolinha.perform()
         acao_bolinha.click()
         acao_bolinha.perform()
-        time.sleep(5)
+        #time.sleep(5)
 
         #PEGAR NUMERO
         numero_cliente = driver.find_element(By.XPATH,contato_cli)
         telefone = numero_cliente.text
         print('telefone e: ',telefone)
-        time.sleep(5)
+        #time.sleep(5)
 
         #PEGAR MSG
         todas_msg = driver.find_elements(By.CLASS_NAME,msg_cliente)
         todas_msg_texto = [e.text for e in todas_msg]
         msg = todas_msg_texto[-1]
         print('ultima mensagem: ',msg)
-        time.sleep(5)
+        #time.sleep(5)
 
         #RESPONDENDO CLIENTE
         campo_de_texto = driver.find_element(By.XPATH,caixa_msg)
         campo_de_texto.click()
         resposta = requests.get("http://localhost/chatbot/index.php", params={'msg': {msg},'telefone':{telefone}})
         botresposta = resposta.text
-        time.sleep(1)
+        #time.sleep(1)
         campo_de_texto.send_keys(botresposta, Keys.ENTER)
+
+        #fecha o contato
+        webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
         
     except:
         print("Esperando novas mensagens...")
