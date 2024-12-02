7e95d521d7198b42332e4f02cc09b2839f2ec653
Otaviano Cruz Gouveia de Arruda Neto <0t4v14n0@gmail.com>
2024-12-02 07:57:25 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,32 +2,54 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+
 import os
 import time
 import requests
 
-#facilitar munutencao
-bolinha_not    = ('x7h3shv')
-contato_cli    = ('//*[@id="main"]/header/div[2]/div/div/div/span')
-msg_cliente    = ('_akbu')
-caixa_msg      = ('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
-caixa_msg2     = ('div[title="Digite uma mensagem"]')
-caixa_pesquisa = ('div[title="Caixa de texto de pesquisa"]')
+# API
+agent = {"User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+api_url = "https://editacodigo.com.br/index/api-whatsapp/xgLNUFtZsAbhZZaxkRh5ofM6Z0YIXwwv" #API PUBLICA
 
-#se nao estiver criada, criarar uma pasta para o armazenamento das informacoes do zap
-#para que nao fique colocando o QR code direto
-dir_path = os.getcwd()
-chrome_options2 = Options()
-chrome_options2.add_argument(r"user-data-dir=" + dir_path + "/zap/sessao")
-driver = webdriver.Chrome(options=chrome_options2)
-driver.get('https://web.whatsapp.com/')
-time.sleep(5)
 
-def bot():
+def obter_dados_api():
+    api = requests.get(api_url, headers=agent).text
+    return api.split(".n.")
+
+def iniciar_driver():
+
+    dir_path = os.getcwd()
+
+    chrome_options = Options()
+
+    #dir_path = "/zap"  # Diretório copiado no Dockerfile
+    sessao_path = os.path.join(r"user-data-dir="+ dir_path + "/zap/sessao")
+    chrome_options.add_argument(r"user-data-dir="+ dir_path + "/zap/sessao")
+    # chrome_options.add_argument("--headless")  # Executa o Chrome em modo headless
+    # chrome_options.add_argument("--no-sandbox")  # Necessário para ambientes sem sandbox
+    # chrome_options.add_argument("--disable-dev-shm-usage")  # Necessário em ambientes com pouco espaço de memória compartilhada
+    # chrome_options.add_argument("--disable-gpu")  # Desabilita a aceleração de GPU
+    # chrome_options.add_argument("--window-size=1920,1080")  # Tamanho da janela
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('https://web.whatsapp.com/')
+    time.sleep(10)
+
+    return driver
+
+def bot(driver):
     try:
+
+        # Obter dados da API
+        api_data = obter_dados_api()
+        bolinha_notificacao = api_data[3].strip()
+        contato_cliente = api_data[4].strip()
+        caixa_msg = api_data[5].strip() #//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]/p
+        msg_cliente = api_data[6].strip()
+
         #PEGAR BOLINHA
-        bolinha = driver.find_element(By.CLASS_NAME,bolinha_not)
-        bolinha = driver.find_elements(By.CLASS_NAME,bolinha_not)
+        bolinha = driver.find_element(By.CLASS_NAME,bolinha_notificacao)
+        bolinha = driver.find_elements(By.CLASS_NAME,bolinha_notificacao)
         clica_bolinha = bolinha[-1]
         acao_bolinha = webdriver.common.action_chains.ActionChains(driver)
         acao_bolinha.move_to_element_with_offset(clica_bolinha,0,-20)
@@ -35,27 +57,22 @@ def bot():
         acao_bolinha.perform()
         acao_bolinha.click()
         acao_bolinha.perform()
-        #time.sleep(5)
 
         #PEGAR NUMERO
-        numero_cliente = driver.find_element(By.XPATH,contato_cli)
+        numero_cliente = driver.find_element(By.XPATH,contato_cliente)
         telefone = numero_cliente.text
-        print('telefone e: ',telefone)
-        #time.sleep(5)
 
         #PEGAR MSG
         todas_msg = driver.find_elements(By.CLASS_NAME,msg_cliente)
         todas_msg_texto = [e.text for e in todas_msg]
         msg = todas_msg_texto[-1]
-        print('ultima mensagem: ',msg)
-        #time.sleep(5)
 
         #RESPONDENDO CLIENTE
-        campo_de_texto = driver.find_element(By.XPATH,caixa_msg)
+        resposta = requests.get("http://localhost/bot/index.php", params={'msg': {msg},'telefone':{telefone}})
+
+        campo_de_texto = driver.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]/p')
         campo_de_texto.click()
-        resposta = requests.get("http://localhost/chatbot/index.php", params={'msg': {msg},'telefone':{telefone}})
         botresposta = resposta.text
-        #time.sleep(1)
         campo_de_texto.send_keys(botresposta, Keys.ENTER)
 
         #fecha o contato
@@ -64,5 +81,17 @@ def bot():
     except:
         print("Esperando novas mensagens...")
 
-while True:
-    bot()
+def botConfirmaPagamento(driver):
+    print()
+
+def botAviso(driver):
+    print()
+
+
+if __name__ == "__main__":
+
+    driver = iniciar_driver()
+
+    while True:
+        bot(driver)
+        time.sleep(3)
